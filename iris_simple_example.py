d81267d2bdc2462cbdbe4cd8298cc2a9411bf9cb
Blaine Bateman <eafpres@gmail.com>
2022-01-24 12:17:47 -0700

    Refactor and fix some issues in demo code

--- iris_simple_example.py
+++ iris_simple_example.py
@@ -15,6 +15,7 @@ from cleanlab.classification import LearningWithNoisyLabels
 from cleanlab.noise_generation import generate_noisy_labels
 from cleanlab.util import value_counts
 from cleanlab.latent_algebra import compute_inv_noise_matrix
+import matplotlib.pyplot as plt
 #
 #%% notes
 #
@@ -31,122 +32,125 @@ from cleanlab.latent_algebra import compute_inv_noise_matrix
 #
 #%% iris data
 #
-# Seed for reproducibility
-seed = 2
-
-np.random.seed(seed=seed)
-
+# seed for reproducibility
+#
+random_seed = 42
+np.random.seed(seed = random_seed)
+#
 # Get iris dataset
+#
 iris = datasets.load_iris()
-X = iris.data  # we only take the first two features.
+X = iris.data
 y = iris.target
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+#
+# split data
+#
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
-try:
-#    get_ipython().run_line_magic('matplotlib', 'inline')
-    from matplotlib import pyplot as plt
-
-    _ = plt.figure(figsize=(12, 8))
-    color_list = plt.cm.tab10(np.linspace(0, 1, 6))
-    _ = plt.scatter(X_train[:, 1], X_train[:, 3],
-                    color=[color_list[z] for z in y_train], s=50)
-    ax = plt.gca()
-    #     plt.axis('off')
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    _ = ax.get_xaxis().set_ticks([])
-    _ = ax.get_yaxis().set_ticks([])
-    _ = plt.title("Iris dataset (feature 3 vs feature 1)", fontsize=30)
-except Exception as e:
-    print(e)
-    print("Plotting is only supported in an iPython interface.")
-
-
+_ = plt.figure(figsize=(12, 8))
+color_list = plt.cm.tab10(np.linspace(0, 1, 6))
+_ = plt.scatter(X_train[:, 1], X_train[:, 3],
+                color=[color_list[z] for z in y_train], s=50)
+ax = plt.gca()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+_ = ax.get_xaxis().set_ticks([])
+_ = ax.get_yaxis().set_ticks([])
+_ = plt.title("Iris dataset (feature 3 vs feature 1)", fontsize=30)
+#
 #%% add noise
 #
-# Generate lots of noise.
+# Generate lots of noise
+#
 noise_matrix = np.array([
     [0.5, 0.0, 0.0],
     [0.5, 1.0, 0.5],
     [0.0, 0.0, 0.5],
 ])
-
+#
 py = value_counts(y_train)
+#
 # Create noisy labels
+#
 s = generate_noisy_labels(y_train, noise_matrix)
-
-try:
-#    get_ipython().run_line_magic('matplotlib', 'inline')
-    from matplotlib import pyplot as plt
-
-    _ = plt.figure(figsize=(15, 8))
-    color_list = plt.cm.tab10(np.linspace(0, 1, 6))
-    for k in range(len(np.unique(y_train))):
-        X_k = X_train[y_train == k]  # data for class k
-        _ = plt.scatter(
-            X_k[:, 1],
-            X_k[:, 3],
-            color=[color_list[noisy_label] for noisy_label in s[y_train == k]],
-            s=200,
-            marker=r"${a}$".format(a=str(k)),
-            linewidth=1,
-        )
+#
+_ = plt.figure(figsize=(15, 8))
+color_list = plt.cm.tab10(np.linspace(0, 1, 6))
+for k in range(len(np.unique(y_train))):
+    X_k = X_train[y_train == k]  # data for class k
     _ = plt.scatter(
-        x=X_train[s != y_train][:, 1],
-        y=X_train[s != y_train][:, 3],
-        color=[color_list[z] for z in s],
-        s=400,
-        facecolors='none',
-        edgecolors='black',
-        linewidth=2,
-        alpha=0.5,
+        X_k[:, 1],
+        X_k[:, 3],
+        color=[color_list[noisy_label] for noisy_label in s[y_train == k]],
+        s=200,
+        marker=r"${a}$".format(a=str(k)),
+        linewidth=1,
     )
-    ax = plt.gca()
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    _ = ax.get_xaxis().set_ticks([])
-    _ = ax.get_yaxis().set_ticks([])
-    _ = plt.title("Iris dataset (features 3 and 1). Label errors circled.",
-                  fontsize=30)
-except Exception as e:
-    print(e)
-    print("Plotting is only supported in an iPython interface.")
-
-
-# In[4]:
-
+_ = plt.scatter(
+    x=X_train[s != y_train][:, 1],
+    y=X_train[s != y_train][:, 3],
+    color=[color_list[z] for z in s],
+    s=400,
+    facecolors='none',
+    edgecolors='black',
+    linewidth=2,
+    alpha=0.5,
+)
+ax = plt.gca()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+_ = ax.get_xaxis().set_ticks([])
+_ = ax.get_yaxis().set_ticks([])
+_ = plt.title("Iris dataset (features 3 and 1). Label errors circled.",
+              fontsize=30)
+#
+#%% show different model results
+#
 print('WITHOUT confident learning, NO noise', end=" ")
-clf = LogisticRegression(solver='lbfgs', multi_class='auto', max_iter=1000, n_jobs = 1)
+clf = LogisticRegression(solver = 'lbfgs',
+                         multi_class = 'auto',
+                         max_iter = 1000,
+                         n_jobs = None)
 _ = clf.fit(X_train, y_train)
 pred = clf.predict(X_test)
 print("Iris dataset test accuracy:", round(accuracy_score(pred, y_test), 2))
-
+#
 print('WITHOUT confident learning, with noise', end=" ")
-clf = LogisticRegression(solver='lbfgs', multi_class='auto', max_iter=1000, n_jobs = 1)
+clf = LogisticRegression(solver = 'lbfgs',
+                         multi_class = 'auto',
+                         max_iter = 1000, n_jobs = None)
 _ = clf.fit(X_train, s)
 pred = clf.predict(X_test)
 print("Iris dataset test accuracy:", round(accuracy_score(pred, y_test), 2))
-
+#
 print("\nNow we show improvement using cleanlab to characterize the noise")
 print("and learn on the data that is (with high confidence) labeled correctly.")
 print()
+#
 print('WITH confident learning (noise matrix given),', end=" ")
-rp = LearningWithNoisyLabels(clf=clf, seed=seed, n_jobs = 1)
-_ = rp.fit(X_train, s, noise_matrix=noise_matrix)
+rp = LearningWithNoisyLabels(clf = clf,
+                             seed = random_seed,
+                             n_jobs = None)
+_ = rp.fit(X_train, s, noise_matrix = noise_matrix)
 pred = rp.predict(X_test)
 print("Iris dataset test accuracy:", round(accuracy_score(pred, y_test), 2))
-
+#
 print('WITH confident learning (noise / inverse noise matrix given),', end=" ")
 inv = compute_inv_noise_matrix(py, noise_matrix)
-_ = rp.fit(X_train, s, noise_matrix=noise_matrix, inverse_noise_matrix=inv)
+_ = rp.fit(X_train, s, noise_matrix = noise_matrix,
+           inverse_noise_matrix = inv)
 pred = rp.predict(X_test)
 print("Iris dataset test accuracy:", round(accuracy_score(pred, y_test), 2))
-
+#
 print('WITH confident learning noise not given,', end=" ")
-clf = LogisticRegression(solver='lbfgs', multi_class='auto', max_iter=1000)
-rp = LearningWithNoisyLabels(clf=clf, seed=seed)
-_ = rp.fit(X_train, s)
-pred = rp.predict(X_test)
+clf = LogisticRegression(solver = 'lbfgs',
+                         multi_class = 'auto',
+                         max_iter = 1000,
+                         n_jobs = None)
+rp1 = LearningWithNoisyLabels(clf = clf,
+                             seed = random_seed)
+_ = rp1.fit(X_train, s)
+pred = rp1.predict(X_test)
 print("Iris dataset test accuracy:", round(accuracy_score(pred, y_test), 2))
 
 
